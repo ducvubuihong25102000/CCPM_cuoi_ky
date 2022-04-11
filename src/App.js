1745ddaa9e42758ad558636e799a22f52893e796
thanhgit2k <thanhgit2000@gmail.com>
2022-04-11 16:03:26 +0700

    [DONE] route page admin dashboard and admin login, new folder Admin in Template

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Chart from "./Template/Chart";
 import Home from "./Template/Home/Home"
 // import Login from "./components/Login1/Login";//
-
-
-
+import AdminPage from "./components/Admin/Dashboard/Dashboard";
+import AdminScreen from "./Template/Admin";
+import Login from "./components/Admin/Login/Login";
+import Admin from "./Template/Admin";
 
 
 function App() {
@@ -14,6 +15,8 @@ function App() {
 			<Switch>
 				<Home exact path="/home" Component={Chart} />
 				<Home exact path="/" Component={Chart} />
+				<Route exact path="/login" Component={Login} />
+				<AdminScreen exact path="/admin/dashboard" Component={AdminPage} />
 			</Switch>
 		</BrowserRouter>
 	);
