96c059cd84272764424bae3109c78c8493474b94
thanhgit2k <thanhgit2000@gmail.com>
2022-04-11 15:46:05 +0700

    [DONE] route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Chart from "./Template/Chart";
 import Home from "./Template/Home/Home"
 // import Login from "./components/Login1/Login";//
+import AdminPage from "./components/Admin/Dashboard";
+import AdminScreen from "./components/Admin/Login";
+
 
 
 
@@ -12,6 +15,8 @@ function App() {
 			<Switch>
 				<Home exact path="/home" Component={Chart} />
 				<Home exact path="/" Component={Chart} />
+				<AdminPage exact path ="/admin/dashboard" Component = {AdminPage}/>
+				<Route exact path="/login" Component={Login}/>
 			</Switch>
 		</BrowserRouter>
 	);
